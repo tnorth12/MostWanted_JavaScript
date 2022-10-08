54fd705ece596f3236917d6f500ced1e9263f54d
Your Name <you@example.com>
2022-10-07 21:18:03 -0600

    Added infolookup tool search functions.

--- most-wanted/app.js
+++ most-wanted/app.js
@@ -18,144 +18,81 @@ function app(peopleArray) {
   }
 }
 
-/**
- * After finding a single person, we pass in the entire person-object that we found,
- * as well as the entire original dataset of people. We need people in order to find
- * descendants and other information that the user may want.
- * @param {Object[]} person     A singular object inside of an array.
- * @param {Array} people        A collection of person objects.
- * @returns {String}            The valid string input retrieved from the user.
- */
-function mainMenu(person, people) {
-    // A check to verify a person was found via searchByName() or searchByTrait()
-    if (!person[0]) {
-        alert("Could not find that individual.");
-        // Restarts app() from the very beginning
-        return app(people);
+let personsSpouse = 0
+
+function infoLookUpTool(person) {
+  console.log("First Name: " + person.firstName);
+  console.log("Last Name: " + person.lastName);
+  console.log("ID Number: " + person.id);
+  console.log("Gender: " + person.gender);
+  console.log("Date of Birth: " + person.dob);
+  console.log("Height: " + person.height);
+  console.log("Weight: " + person.weight);
+  console.log("Eye Color: " + person.eyeColor);
+  console.log("Occupation: " + person.occupation);
+  if (person.parents[0] == null) {
+    console.log("No Parents found")
+  }
+  else if (person.parents[0] !== null && person.parents[1] == null)
+    console.log("One parent found. Parent:" + person.parents[0])
+  else {
+    console.log("Parent 1: " + person.parents[0], "Parent 2: " + person.parents[1]);
+  }
+  if (person.currentSpouse == null) {
+    console.log("No Spouse found")
+  }
+  else {
+    console.log("Current Spouse: " + person.currentSpouse);
+  }
+}
+
+function getPerson(id, peopleArray){
+  let person = peopleArray.filter(function(el){
+    if(el.id == id){
+      return true;
+    }else{
+      return false;
     }
-    let displayOption = prompt(
-        `Found ${person[0].firstName} ${person[0].lastName}. Do you want to know their 'info', 'family', or 'descendants'?\nType the option you want or type 'restart' or 'quit'.`
-    );
-    // Routes our application based on the user's input
-    switch (displayOption) {
-        case "info":
-            //! TODO #1: Utilize the displayPerson function //////////////////////////////////////////
-            // HINT: Look for a person-object stringifier utility function to help
-            let personInfo = displayPerson(person[0]);
-            alert(personInfo);
-            break;
-        case "family":
-            //! TODO #2: Declare a findPersonFamily function //////////////////////////////////////////
-            // HINT: Look for a people-collection stringifier utility function to help
-            let personFamily = findPersonFamily(person[0], people);
-            alert(personFamily);
-            break;
-        case "descendants":
-            //! TODO #3: Declare a findPersonDescendants function //////////////////////////////////////////
-            // HINT: Review recursion lecture + demo for bonus user story
-            let personDescendants = findPersonDescendants(person[0], people);
-            alert(personDescendants);
-            break;
-        case "restart":
-            // Restart app() from the very beginning
-            app(people);
-            break;
-        case "quit":
-            // Stop application execution
-            return;
-        default:
-            // Prompt user again. Another instance of recursion
-            return mainMenu(person, people);
-    }
+  });
+  return person[0];
 }
-// End of mainMenu()
 
-/**
- * This function is used when searching the people collection by
- * a person-object's firstName and lastName properties.
- * @param {Array} people        A collection of person objects.
- * @returns {Array}             An array containing the person-object (or empty array if no match)
- */
-function searchByName(people) {
-    let firstName = promptFor("What is the person's first name?", chars);
-    let lastName = promptFor("What is the person's last name?", chars);
-
-    // The foundPerson value will be of type Array. Recall that .filter() ALWAYS returns an array.
-    let foundPerson = people.filter(function (person) {
-        if (person.firstName === firstName && person.lastName === lastName) {
-            return true;
-        }
-    });
-    return foundPerson;
+function getSpouse(person, peopleArray){ 
+  if(person.currentSpouse == null){
+    console.log("This person has no spouse.");
+  }
+  else{
+    let spouse = getPerson(person.currentSpouse, peopleArray);
+    console.log(`Spouse: ${spouse.firstName} ${spouse.lastName}`);
+  }
 }
-// End of searchByName()
 
-/**
- * This function will be useful for STRINGIFYING a collection of person-objects
- * first and last name properties in order to easily send the information
- * to the user in the form of an alert().
- * @param {Array} people        A collection of person objects.
- */
-function displayPeople(people) {
-    alert(
-        people
-            .map(function (person) {
-                return `${person.firstName} ${person.lastName}`;
-            })
-            .join("\n")
-    );
+function getParents(person, peopleArray){
+  let parentOne = getPerson(person.parents[0], peopleArray)
+  let parentTwo = getPerson(person.parents[1], peopleArray)
+  if(person.parents.length < 1){
+    console.log("This person has no parents");
+  }
+  else if(person.parents[1]==null){ 
+    console.log(`Parents: ${parentOne.firstName} ${parentOne.lastName}`); 
+  }
+  else{
+    console.log(`Parents: ${parentOne.firstName} ${parentOne.lastName} , ${parentTwo.firstName} ${parentTwo.lastName}`)
+  }
 }
-// End of displayPeople()
 
-/**
- * This function will be useful for STRINGIFYING a person-object's properties
- * in order to easily send the information to the user in the form of an alert().
- * @param {Object} person       A singular object.
- */
-function displayPerson(person) {
-    let personInfo = `First Name: ${person.firstName}\n`;
-    personInfo += `Last Name: ${person.lastName}\n`;
-    //! TODO #1a: finish getting the rest of the information to display //////////////////////////////////////////
-    alert(personInfo);
+function getDescendants(person, peopleArray){
+ let children = peopleArray.filter(function(el){
+   if(el.parents.includes(person.id)){
+     return true;
+   }
+   else{
+     return false;
+   }
+ });
+for(let i =0; i < children.length; i++){
+  children.concat(getDescendants(children[i],peopleArray));
 }
-// End of displayPerson()
+  return children;
+}  
 
-/**
- * This function's purpose is twofold:
- * First, to generate a prompt with the value passed in to the question parameter.
- * Second, to ensure the user input response has been validated.
- * @param {String} question     A string that will be passed into prompt().
- * @param {Function} valid      A callback function used to validate basic user input.
- * @returns {String}            The valid string input retrieved from the user.
- */
-function promptFor(question, valid) {
-    do {
-        var response = prompt(question).trim();
-    } while (!response || !valid(response));
-    return response;
-}
-// End of promptFor()
-
-/**
- * This helper function checks to see if the value passed into input is a "yes" or "no."
- * @param {String} input        A string that will be normalized via .toLowerCase().
- * @returns {Boolean}           The result of our condition evaluation.
- */
-function yesNo(input) {
-    return input.toLowerCase() === "yes" || input.toLowerCase() === "no";
-}
-// End of yesNo()
-
-/**
- * This helper function operates as a default callback for promptFor's validation.
- * Feel free to modify this to suit your needs.
- * @param {String} input        A string.
- * @returns {Boolean}           Default validation -- no logic yet.
- */
-function chars(input) {
-    return true; // Default validation only
-}
-// End of chars()
-
-//////////////////////////////////////////* End Of Starter Code *//////////////////////////////////////////
-// Any additional functions can be written below this line 👇. Happy Coding! 😁
